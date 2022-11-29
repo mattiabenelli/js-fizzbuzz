954a77d7de4153669eb24a6a45c6e3d98adbd305
mattiabenelli <benellimattia0@gmail.com>
2022-11-29 16:50:35 +0100

    bonus completi

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
+const container = document.querySelector('.container')
+
 for (let i=1; i < 100; i++){
+    let element;
     if(i % 5 == 0 && i % 3 == 0){
         console.log(`FizzBuzz`);
+        element = `<div class="box box-fizzbuzz">FizzBuzz</div>`;
     }
     else if(i % 3 == 0){
         console.log(`Fizz`);
+        element = `<div class="box box-fizz">Fizz</div>`;
     }
     else if(i % 5 == 0){
         console.log(`Buzz`);
+        element = `<div class="box box-buzz">Buzz</div>`;
     }
     else{
-        console.log(i)
+        console.log(i);
+        element = `<div class="box box-number">${i}</div>`;
     }
+    container.innerHTML += element;
 }
